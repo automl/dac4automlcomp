32d7b51d8cdfb94e760675aa7a1d572b11dc06cd
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-03-18 13:35:34 +0100

    Add path argument to load_solution signature
    The ingestion program passes the path to the submission directory.

--- submission_template/solution.py
+++ submission_template/solution.py
@@ -1,13 +1,20 @@
+from pathlib import Path
+
 from dac4automlcomp.policy import DACPolicy
 
 
-def load_solution() -> DACPolicy:
+def load_solution(path: Path = Path(".")) -> DACPolicy:
     """
     Load Solution.
 
     Serves as an entry point for the competition evaluation.
 
     Place your code loading your policy here.
+
+    Parameters
+    ----------
+    path: Path
+        The path to your submission directory.
 
     Returns
     -------
