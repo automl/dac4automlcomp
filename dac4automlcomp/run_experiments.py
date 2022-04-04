2b52e843f47e7fea64fe99f216843bc6e93ce954
Raghu Rajan <raghuspacerajan@gmail.com>
2022-04-04 16:37:01 +0200

    Latest ingestion

--- dac4automlcomp/run_experiments.py
+++ dac4automlcomp/run_experiments.py
@@ -14,36 +14,36 @@ from os.path import join
 
 
 def run_experiment(
-    dac_policy_obj,
+    dac_policy_loader,
     dac_env_obj,
     gen_seed,
     num_instances,
     policy_seed,
     time_limit_sec,
+    comp_track,
     **kwargs,
 ):
     """
     This is the main experiment runner for the DAC4AutoML competition tracks.
-    It takes an object of the DAC policy to be evaluated and tests the policy on
+    It takes a loader loading the DAC policy to be evaluated and tests the policy on
     a set of num_instances target problem instances and returns the resulting performances as a Numpy array.
 
     #TODO Improve docstrings
     # defining train/test splits and the distribution for the contexts of the DACEnv.
 
     Args:
-        dac_policy_obj (DACPolicy):
-        dac_env_obj (DACEnv):
-        gen_seed (int):
-        num_instances (int):
-        policy_seed (int):
-        time_limit_sec (int):
+        dac_policy_loader (Callable[[], DACPolicy]): Loads the policy to be evaluated (reloaded for every instance)
+        dac_env_obj (DACEnv): The evaluation environment
+        gen_seed (int): The seed determining the order of the infinite sequence of evaluation instances
+        num_instances (int): The prefix of instances to evaluate the loaded policy on
+        policy_seed (int): The seed determining the random seed for the policy's rng
+        time_limit_sec (int): The wall-clock time limit for the entire evaluation
 
     Returns:
         total_rewards: A Numpy array of performances with shape (num_instances,)
+        duration (float): Duration of the evaluation in sec
 
     """
-    # TODO: Exclude downloading the datasets in the evaluation time?
-    start_time = time.time()  # TODO: Replace with a superior way of timing?
 
     screen_output_width = 80 # os.get_terminal_size().columns
     repeat_equal_sign = (screen_output_width - 24) // 2
@@ -52,48 +52,80 @@ def run_experiment(
     print(
         set_ansi_escape
         + "=" * repeat_equal_sign
-        + "Running DAC4AutoML experiment"
+        + "Running Evaluation"
         + "=" * repeat_equal_sign
         + reset_ansi_escape
     )
-    print("\n\nLoaded object of type:", type(dac_policy_obj), "\n\n")
+
     print("Current working directory:", os.getcwd())
 
     total_rewards = np.zeros((num_instances,))
+    if comp_track == "dac4rl":
+        per_env_stats = {}
+        curr_env_type = None
+        all_envs = ["CARLPendulumEnv", "CARLAcrobotEnv", "CARLMountainCarContinuousEnv", "CARLLunarLanderEnv", "CARLCartPoleEnv"]
+        for env in all_envs:
+            per_env_stats[env] = {}
+            per_env_stats[env]["reward"] = 0.0
+            per_env_stats[env]["num_instances"] = 0
+
     dac_env_obj.seed(gen_seed)
     policy_seed_rng = np.random.RandomState(policy_seed)
 
+    start_time = time.time()  # TODO: Replace with a superior way of timing?
+    duration = 0
     for i in range(num_instances):
-        if time.time() - start_time < time_limit_sec:
-            # TODO: To avoid stateful policies, we should actually re-load the policy here!
-            # (and optionally pass a policy_loader and policy_loader_kwargs(?) as argument instead)
+        print("> Start evaluation on instance {}:".format(i))
+        if duration < time_limit_sec:
+            dac_policy_obj = dac_policy_loader()
+            print("- Loaded policy object of type:", type(dac_policy_obj))
             dac_policy_obj.seed(
                 policy_seed_rng.randint(1, np.iinfo(np.int64).max, dtype=np.int64)
             )
             obs = dac_env_obj.reset()
+            if comp_track == "dac4rl":
+                curr_env_type = dac_env_obj.current_instance.env_type
+
+                # If an env has hit its limit of num_instances, keep resetting until another env instance is sampled
+                while per_env_stats[curr_env_type]["num_instances"] == num_instances / 5:
+                    obs = dac_env_obj.reset()
+                    curr_env_type = dac_env_obj.current_instance.env_type
+
+                per_env_stats[curr_env_type]["num_instances"] += 1
+
             print(
                 set_ansi_escape
                 + "\nInstance set to: "
-                + dac_env_obj.current_instance.dataset if args.competition_track == "dac4sgd" else dac_env_obj.current_instance.env_type
+                + (dac_env_obj.current_instance.dataset if comp_track == "dac4sgd" else dac_env_obj.current_instance.env_type)
                 + reset_ansi_escape
             )
+
+
+                
             dac_policy_obj.reset(dac_env_obj.current_instance)
             done = False
             while not done:
                 config = dac_policy_obj.act(obs)
                 obs, reward, done, info = dac_env_obj.step(config)
                 total_rewards[i] += reward
-                #TODO Make it AUC for RL track
-        else:
-            # TODO: generate some warning that time budget has been exceeded
+
+
+        duration = time.time() - start_time
+        if duration > time_limit_sec:
             warnings.warn(
                 "TIME LIMIT EXCEEDED. Setting total reward for instance "
                 + str(i)
-                + " to 0."
+                + " to -1e6."
             )
-            total_rewards[i] = -np.inf
+            total_rewards[i] = -1e6
+        
+        if comp_track == "dac4rl":
+            per_env_stats[curr_env_type]["reward"] += total_rewards[i]
 
-    return total_rewards
+        print("- Total reward for instance: {}".format(total_rewards[i]))
+        print("- Time elapsed: {} / {} sec".format(duration, time_limit_sec))
+
+    return total_rewards, duration, per_env_stats if comp_track == "dac4rl" else None
 
 
 if __name__ == "__main__":
@@ -101,14 +133,14 @@ if __name__ == "__main__":
     root_dir = getcwd()
 
     parser = argparse.ArgumentParser(
-        description="The experiment runner for the DAC4RL track."
+        description="The experiment runner for the DAC4AutoML competition."
     )
     parser.add_argument(
         "-t",
-        "--competition-track", 
-        choices=['dac4sgd', 'dac4rl'], 
-        help="DAC4SGD or DAC4RL", 
-        default="dac4sgd",
+        "--competition-track",
+        choices=['dac4sgd', 'dac4rl'],
+        help="DAC4SGD or DAC4RL",
+        default="dac4rl",
     )
     parser.add_argument(
         "-s",
@@ -131,11 +163,24 @@ if __name__ == "__main__":
         default="",
         help="",
     )
+    parser.add_argument(
+        "-g",
+        "--gen-seed",
+        type=int,
+        default=666,
+        help="The generator seed (determining the instance order)",
+    )
+    parser.add_argument(
+        "-n",
+        "--n_instances",
+        type=int,
+        default=5,
+        help="The number of instances to evaluate on, when set overrides the track defaults",
+    )
 
     print("Working directory:", root_dir + "\n")
     args, unknown = parser.parse_known_args()
-    # TODO: Should be cmd arguments
-
+    print("Track:", args.competition_track + "\n")
 
     # os.environ['OPENBLAS_NUM_THREADS'] = '1'
     # print("os.environ:", os.environ)
@@ -144,14 +189,18 @@ if __name__ == "__main__":
 
     print("pip installing packages...\n")
     os.system("pip install -r " + args.submission_dir + "/requirements.txt")
+    # from dotenv import load_dotenv
+    # load_dotenv("/app/codalab/dac4automl/.env")
 
-    os.environ['HTTP_PROXY'] = "http://web-proxy.rrzn.uni-hannover.de:3128"
-    os.environ['HTTPS_PROXY'] = "http://web-proxy.rrzn.uni-hannover.de:3128"
-    # print("os.environ:", os.environ)
+    # os.environ['HTTP_PROXY'] = "http://web-proxy.rrzn.uni-hannover.de:3128"
+    # os.environ['HTTPS_PROXY'] = "http://web-proxy.rrzn.uni-hannover.de:3128"
+    print("os.environ:", os.environ)
+    print(os.system("cat /proc/cpuinfo"))
+    print(os.system("cat /proc/meminfo"))
     # print(os.system("ls -R /app/codalab/dac4automl/"))
 
-    # os.system("bash " + args.ingestion_dir + "/evaluate_submission.sh -i " + 
-    #             args.ingestion_dir + " -d " + args.submission_dir + " -f solution.py -o " + 
+    # os.system("bash " + args.ingestion_dir + "/evaluate_submission.sh -i " +
+    #             args.ingestion_dir + " -d " + args.submission_dir + " -f solution.py -o " +
     #             args.output_dir + " 2>&1")
 
     ingestion_dir = os.path.abspath(args.ingestion_dir)
@@ -169,33 +218,25 @@ if __name__ == "__main__":
         print("Using submission_dir: " + submission_dir + "\n")
 
 
-    #TODO Remove?
-    with open(submission_dir + '/metadata', 'r') as fh:
-        track = fh.readline()
-        if "rltrack" in track:
-            args.competition_track = "dac4rl"
-
-
     path.insert(0, args.submission_dir)
     # print("path:", path)
 
     from solution import load_solution
 
-    num_instances = 10
     args_ml = {
         "env_name": "sgd-v0",
-        "gen_seed": 666,
-        "policy_seed": 42,
-        "num_instances": num_instances,
-        "time_limit_sec": 21_600,
+        "gen_seed": args.gen_seed,
+        "policy_seed": args.gen_seed,
+        "num_instances": 80 if args.n_instances is None else args.n_instances,
+        "time_limit_sec": 3_600,
     }
 
     args_rl = {
         "env_name": "dac4carl-v0",
-        "gen_seed": 666,
-        "policy_seed": 42,
-        "num_instances": num_instances,
-        "time_limit_sec": 7_200,
+        "gen_seed": args.gen_seed,
+        "policy_seed": args.gen_seed,
+        "num_instances": 5 if args.n_instances is None else args.n_instances,
+        "time_limit_sec": 10_800,
     }
 
     if args.competition_track == "dac4sgd":
@@ -205,23 +246,37 @@ if __name__ == "__main__":
         env_args = args_rl
         import rlenv
 
-    from pathlib import Path    
-    policy = load_solution(path=Path(args.submission_dir)) #TODO assert it's a DACPolicy
+    from pathlib import Path
+    policy_loader = lambda : load_solution(path=Path(args.submission_dir))  # TODO assert it's a DACPolicy
 
-    env = gym.make(env_args["env_name"])
+    env = gym.make(env_args["env_name"], total_timesteps=1e5)
 
-    total_rewards = run_experiment(policy, env, **env_args)
+    print("num_instances:", args.n_instances)
+
+    total_rewards, duration, per_env_stats = run_experiment(policy_loader, env, comp_track=args.competition_track, **env_args)
+    total_rewards = [1.0/(i+1) if i >= len(total_rewards) else total_rewards[i] for i in range(args.n_instances)]
 
     print("total_rewards:", total_rewards)
+    print("duration:", duration)
 
     # Write scores.txt
-    fout = open(args.output_dir + '/scores.txt', 'a')
+    fout = open(Path(args.output_dir) / Path('scores.txt'), 'w')
     # for rew in total_rewards:
     if args.competition_track == "dac4sgd":
-        fout.write("DLSCORES: " + str(np.mean(total_rewards)) + '\n')
-        # fout.write("RLSCORES: " + str("NaN") + '\n')
+        for i, r in enumerate(total_rewards):
+            fout.write("cid_{}: {} \n".format(i, -r))
+        fout.write("duration: {} \n".format(duration))
     elif args.competition_track == "dac4rl":
-        # fout.write("DLSCORES: " + str("NaN") + '\n')
-        fout.write("RLSCORES: " + str(np.mean(total_rewards)) + '\n')
+        print("per_env_stats:", per_env_stats)
+        for i, r in enumerate(per_env_stats):
+            all_envs_full = ["CARLPendulumEnv", "CARLAcrobotEnv", "CARLMountainCarContinuousEnv", "CARLLunarLanderEnv", "CARLCartPoleEnv"]
+            all_envs = ["Pendulum", "Acrobot", "MountainCarContinuous", "LunarLander", "CartPole"]
+
+            if per_env_stats[all_envs_full[i]]["num_instances"] != 0:
+                fout.write("{}: {} \n".format(all_envs[i], per_env_stats[all_envs_full[i]]["reward"]/per_env_stats[all_envs_full[i]]["num_instances"]))
+            else:
+                warnings.warn("No instances of env " + all_envs_full[i] + " sampled.")
+
+        fout.write("Duration: {} \n".format(duration))
 
     fout.close()
